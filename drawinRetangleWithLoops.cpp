dc4afff7400e748a0450391731ac09f9f504b52a
Pedro L. Ramos <economy.pedrolucas@hotmail.com>
2025-08-22 14:15:36 -0400

    update

---
+++ drawinRetangleWithLoops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip> // This library is used to format output, for example to set precision or fill characters
+
+using namespace std;
+ 
+int main(){
+
+    int height, width;
+    char symbol;
+
+    cout << "Please, insert the Height: ";
+    cin >> height;
+
+    cout << "Now, please insert the widht: ";
+    cin >> width;
+    
+    cout << "Awesome, now please choose a symbol ";
+    cin >> symbol;
+
+    for (int h = 0; h < height; h++){
+        for (int w = 0; w < width; w++){
+            cout << setw(3) << symbol;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+    
